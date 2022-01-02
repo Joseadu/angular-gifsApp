3b10170ebf87a113477e8bef340c99ed47ee5a1f
Joseadu <jose.uk94@gmail.com>
2022-01-02 12:00:54 +0000

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
+//MODULES
+import { SharedModule } from './shared/shared.module';
+import { GifsModule } from './gifs/gifs.module';
+
+//COMPONENTS
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +14,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    SharedModule,
+    GifsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
